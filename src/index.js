02c9229151e255e82396d27464fddc05a5b2b932
Felipe Bergamin <felipebergamin6@gmail.com>
2019-09-04 20:49:48 -0300

    react navigation

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
 
 import './config/ReactotronConfig';
-
-console.tron.log(`Init at ${new Date()}`);
+import Routes from './routes';
 
 const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="light-content" />
-      <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
-          {__DEV__ ? <Text>Dev</Text> : <Text>Prod</Text>}
-        </ScrollView>
-      </SafeAreaView>
-    </>
-  );
+  return <Routes />;
 };
 
 export default App;
