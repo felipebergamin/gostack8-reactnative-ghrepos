4955722953d2d663624db005a3b23e46dcb8905c
Felipe Bergamin <felipebergamin6@gmail.com>
2019-09-04 20:14:57 -0300

    eslint, prettier, reactotron

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { SafeAreaView, ScrollView, Text, StatusBar } from 'react-native';
 
+import './config/ReactotronConfig';
+
+console.tron.log(`Init at ${new Date()}`);
+
 const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" />
       <SafeAreaView>
         <ScrollView contentInsetAdjustmentBehavior="automatic">
-          <Text>Teste</Text>
+          {__DEV__ ? <Text>Dev</Text> : <Text>Prod</Text>}
         </ScrollView>
       </SafeAreaView>
     </>
